a140ad10977fece3e2950307ad1d620f9a756466
waseem tasawar <waseemtasawar97@gmail.com>
2024-07-11 00:07:24 +0500

    New component addition

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import NewProject from "./component/NewProject";
 import ProjectSliderBar from "./component/ProjectSliderBar";
 
 function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
       <ProjectSliderBar />
+      <NewProject />
     </main>
   );
 }
---
+++ src/component/NewProject.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Input from "./Input";
+
+const NewProject = () => {
+  return (
+    <div className="w-[35rem] mt-16">
+      <menu className="flex items-center justify-end gap-4 py-4">
+        <li>
+          <button className="text-stone-800 hover:text-stone-950">
+            Cancle
+          </button>
+        </li>
+        <li>
+          <button className="px-6 pt-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
+            Save
+          </button>
+        </li>
+      </menu>
+      <div>
+        <Input label="Title" />
+        <Input label="Description" textarea />
+        <Input label="Due Date" />
+      </div>
+    </div>
+  );
+};
+
+export default NewProject;

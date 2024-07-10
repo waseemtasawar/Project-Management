cff2c7877f5130200d653ae6b89891940bd136e9
waseem tasawar <waseemtasawar97@gmail.com>
2024-07-10 20:20:53 +0500

    New Project MAnagement System

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ProjectSliderBar from "./component/ProjectSliderBar";
+
+function App() {
+  return (
+    <main className="h-screen my-8">
+      <ProjectSliderBar />
+    </main>
+  );
+}
+
+export default App;
